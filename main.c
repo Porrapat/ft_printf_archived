e8302130609105ec59c7d22cdabf67134c65e73e
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2022-03-05 12:04:12 +0700

    Add More Files

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
-// #include <stdio.h>
+#include <stdio.h>
 #include "ft_printf.h"
 
 int main()
 {
-    // printf("Hello World\n");
+    printf("Hello World\n");
     // printf("%d\n", ft_isalnum('\n'));
     // ft_putstr_fd("Hello\n", 1);
     ft_printf("Hello World\n");
